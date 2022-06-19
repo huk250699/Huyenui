b9cbbd952fa0e5e84acfca86504e72f70074c765
huk250699 <87457168+huk250699@users.noreply.github.com>
2022-06-19 21:49:43 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Huyền';
+const giftUrl = 'https://www.youtube.com/watch?v=RaoBKCKIDAI';
+const eventName = 'người tôi yêu';
+const titleCard = 'Tặng em';
+const contentCard = 'ahihi đồ ngốc ';
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
